fe21f29b764373487d93c476f7c2d9257d0bf6be
skooger <adamjskooglambda@gmail.com>
2019-12-06 09:22:52 -0800

    completed the card component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,54 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+let topics = ['javascript', 'bootstrap', 'technology', 'jquery', 'node']
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(function (response) {
+    // handle success
+    let articles = response['data']['articles']
+    for(let i = 0; i < topics.length; i++){
+
+        //console.log(articles[topics[i]])
+        let topicArticleList = articles[topics[i]]
+        topicArticleList.forEach(element => {
+            console.log(element)
+            let cardContainer = document.querySelector('.cards-container')
+            cardContainer.appendChild(createCard(element))
+        });
+    }
+  })
+
+function createCard(data){
+
+    //create the elements
+    const card = document.createElement('div'),
+        headline = document.createElement('div'),
+        author = document.createElement('div'),
+        imgContainer = document.createElement('div'),
+        img = document.createElement('img'),
+        name = document.createElement('span')
+
+    //set the classes 
+    card.classList.add('card')
+    headline.classList.add('headline')
+    author.classList.add('author')
+    imgContainer.classList.add('img-container')
+
+    //append the children
+    card.appendChild(headline)
+    card.appendChild(author)
+    imgContainer.appendChild(img)
+    card.appendChild(imgContainer)
+    card.appendChild(name)
+
+    //set content
+    headline.textContent = data['headline']
+    img.src = data['authorPhoto']
+    name.textContent = "By " + data['authorName']
+
+
+    return card
+
+}
